9c78a4f9e396133a0c664b144e9c192596bde041
amaazn <anonymousdarkzone1@gmail.com>
2025-06-25 08:32:40 +0000

    object2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,6 +1,5 @@
-// const tinderUser = new Object() constructor method
-
-const tinderUser = {}  // literate
+ // const tinderUser = new Object()
+const tinderUser = {}
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -31,7 +30,7 @@ const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
 
-const users = [       // return from databse
+const users = [
     {
         id: 1,
         email: "h@gmail.com"
@@ -46,8 +45,7 @@ const users = [       // return from databse
     },
 ]
 
-//console.log(users[1].email)
-
+users[1].email
 // console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
@@ -55,3 +53,31 @@ const users = [       // return from databse
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+//json api
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
