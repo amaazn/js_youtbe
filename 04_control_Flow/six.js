1b2d871652703d0c1642230ab3560b60656bacc4
amaazn <anonymousdarkzone1@gmail.com>
2025-06-29 10:42:32 +0000

    6

--- 04_control_Flow/six.js
+++ 04_control_Flow/six.js
@@ -8,13 +8,21 @@
 
 // console.log(values);
 
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     //console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
 // } )
-
-
 
 // const newNums = []
 
@@ -25,8 +33,6 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // } )
 
 // console.log(newNums);
-
-
 
 
 const books = [
